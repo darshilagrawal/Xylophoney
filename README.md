4da28be3c6a33563fced19ac9a9944d4b99698d7
Darshil <61809014+darshilagrawal@users.noreply.github.com>
2021-01-29 10:47:03 +0530

    Xylophone App to Play Sounds

---
+++ README.md
@@ -0,0 +1,23 @@
+
+
+# Xylophone
+
+## Our Goal
+
+The goal is to play sound and use an Apple library called AVFoundation. The most important skill of a great programmer is being able to solve your own problems.
+
+
+ Music apps are so popular on the App Store that they even get their own category.
+
+## What I Learned in this module
+
+* How to play sound using AVFoundation and AVAudioPlayer.
+* Understand Apple documentation and how to use StackOverflow.
+* Functions and methods in Swift.
+* Data types.
+* Swift loops.
+* Variable scope.
+* The ViewController lifecycle.
+* Error handling in Swift.
+* Code refactoring.
+* Basic debugging.
---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    var player = AVAudioPlayer()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+         sender.alpha = 0.5 //sets the opacity to 50%
+           //Code should execute after 0.2 second delay.
+//          print("Start")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+//           print("End")//Bring's sender's opacity back up to fully opaque.
+           sender.alpha = 1.0
+        }
+        playSound(soundName:sender.currentTitle!)
+    }
+    
+    func playSound(soundName:String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+}
