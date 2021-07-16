c6f1d9fbfd6e661861f303804fb0833baf03e79e
darshilagrawal <darshilagrawal2000@gmail.com>
2021-07-16 23:22:06 +0530

    Added User Animations

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -19,6 +19,11 @@ class ViewController: UIViewController {
 //           print("End")//Bring's sender's opacity back up to fully opaque.
            sender.alpha = 1.0
         }
+        UIView.transition(with: sender,
+                                  duration: 0.6,
+                                  options: .transitionFlipFromLeft,
+                                  animations: {
+                }, completion: nil)
         playSound(soundName:sender.currentTitle!)
     }
     
