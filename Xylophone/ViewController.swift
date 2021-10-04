08e7b52e88fe574d01296d633572b6cc798aefbb
darshilagrawal <darshilagrawal2000@gmail.com>
2021-10-04 22:41:50 +0530

    Indentation fix

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,24 +6,23 @@ import AVFoundation
 class ViewController: UIViewController {
     var player = AVAudioPlayer()
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     @IBAction func keyPressed(_ sender: UIButton) {
-         sender.alpha = 0.5 //sets the opacity to 50%
-           //Code should execute after 0.2 second delay.
-//          print("Start")
+        sender.alpha = 0.5 //sets the opacity to 50%
+        //Code should execute after 0.2 second delay.
+        //          print("Start")
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-//           print("End")//Bring's sender's opacity back up to fully opaque.
-           sender.alpha = 1.0
+            //           print("End")//Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
         }
         UIView.transition(with: sender,
-                                  duration: 0.6,
-                                  options: .transitionFlipFromLeft,
-                                  animations: {
-                }, completion: nil)
+                          duration: 0.6,
+                          options: .transitionFlipFromLeft,
+                          animations: {
+        }, completion: nil)
         playSound(soundName:sender.currentTitle!)
     }
     
@@ -31,6 +30,5 @@ class ViewController: UIViewController {
         let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
-                
     }
 }
